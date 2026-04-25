6952f994c0081ad1bf0a01d55e2b52375b53d206
maritims <maritim@gmail.com>
2026-04-26 00:12:31 +0200

    more cmake stuff

---
+++ core/src/list.c
@@ -0,0 +1,44 @@
+#include "../include/core/list.h"
+
+void ctm_list_add(ctm_list_node_t* head, ctm_list_node_t* new_node) {
+    /*
+     * Consider the nodes A and B.
+     * A: the new head.
+     * B: the current head.
+     *
+     * We're pushing everything back to make room for A, take the following steps:
+     * 1) Set the next node of A to the next node of B (to maintain circularity).
+     * 2) Set the previous node of A to B.
+     * 3) Set the previous node (prev) of the last element in the list (head->next) to A.
+     * 4) Set the next node of B to A.
+     */
+
+    new_node->next   = head->next;
+    new_node->prev   = head;
+    head->next->prev = new_node;
+    head->next       = new_node;
+}
+
+void ctm_list_del(ctm_list_node_t* node) {
+    /*
+     * Consider the nodes A, B and C.
+     * A: the previous node
+     * B: the target node
+     * C: the next node
+     *
+     * We're removing B, take the following steps:
+     * 1) Set the previous node of C to A, skipping B.
+     * 2) Set the next node of A to C, skipping B.
+     * 3) Clear B's pointers to prevent dangling references.
+     */
+
+    node->next->prev = node->prev;
+    node->prev->next = node->next;
+    node->next       = NULL;
+    node->prev       = NULL;
+}
+
+void ctm_list_init(ctm_list_node_t* head) {
+    head->next = head;
+    head->prev = head;
+}
---
+++ metrics/include/metrics/ctm_process_manager.h
@@ -0,0 +1,25 @@
+#ifndef CTM_METRICS_PROCESS_MANAGER_H
+#define CTM_METRICS_PROCESS_MANAGER_H
+
+#include "core/list.h"
+#include "metrics/ctm_process_metrics.h"
+
+typedef struct ctm_process_manager ctm_process_manager_t;
+
+typedef void (*ProcessManagerCallback)(ctm_process_metrics_t* process_metrics);
+
+typedef enum {
+    CTM_PROCESS_MANAGER_SUCCESS,
+    CTM_PROCESS_MANAGER_ERR_INVALID_ARG,
+    CTM_PROCESS_MANAGER_ERR_OPEN,
+    CTM_PROCESS_MANAGER_ERR_NOT_FOUND,
+    CTM_PROCESS_MANAGER_ERR_INTERNAL
+} ctm_process_manager_status_t;
+
+ctm_process_manager_status_t process_manager_refresh(ctm_list_node_t*         process_metrics_list,
+                                                     size_t                 process_metrics_arr_len,
+                                                     ProcessManagerCallback on_process_added,
+                                                     ProcessManagerCallback on_process_updated,
+                                                     ProcessManagerCallback on_process_removed);
+
+#endif
